f43d383087239af0516825a47e6ea6653b0a428b
Atheny <chenmiaomiao199305@163.com>
2024-04-02 22:16:37 +0800

    feat: finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,15 @@
 module.exports = {
+    title: 'test-ui',
     base: '/testUI/',
     themeConfig: {
+        nav: [{
+            text: 'Home', link: '/'
+
+        }, {
+            text: 'Github', link: 'https://github.com/Atheny/testUI'
+        }, {
+            text: 'Npm', link: 'https://www.npmjs.com/package/test-ui-atheny'
+        }],
         sidebar: [
             '/',
             '/componentDocs/card'
